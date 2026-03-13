b43e2689525cab8eb696c958dc75d4d486c56fb7
Wendell Fort Dejesus <fortdejesus190@gmail.com>
2026-03-13 14:30:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3574308780-90df0",
+  "appId": "1:885645008087:web:81a0131c280c7ae3fb6492",
+  "apiKey": "AIzaSyDzY0kVEBg5VVahVyDoZH40dqaBT2lZsz0",
+  "authDomain": "studio-3574308780-90df0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "885645008087"
+};

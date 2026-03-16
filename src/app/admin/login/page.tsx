f538e0b5d1b82890611e6282fc1d1c54496b3b72
Wendell Fort Dejesus <fortdejesus190@gmail.com>
2026-03-16 07:06:24 +0000

    show this to the user
    but the login is jcesperanza@neu.edu.ph

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -79,7 +79,7 @@ export default function AdminLoginPage() {
                   <div className="relative">
                     <User className="absolute left-3 top-3 h-5 w-5 text-muted-foreground" />
                     <Input 
-                      placeholder="username@neu.edu.ph" 
+                      placeholder="user@neu.edu.ph" 
                       type="text" 
                       required
                       value={username}
